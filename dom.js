76593b0c0598182c982932830f3cb4962ac17215
Thierno SOUMAH <thsoum>
2019-08-29 17:54:18 +0200

    debut part 3 traversy

--- dom.js
+++ dom.js
@@ -158,3 +158,34 @@
 // newDiv.style.fontSize='30px';
 // console.log(newDiv);
 // container.insertBefore(newDiv, h1);
+
+
+/**---------------------------------------------------------------------------DOM PART 3-------------------------------------  -------------------------- */
+var button = document.getElementById('button').addEventListener('click', buttonClick);
+
+function buttonClick(e){
+    // console.log('button cliked');
+    // document.getElementById('header-title').textContent = 'changed';
+    // document.querySelector('#main').style.backgroundColor= '#f4f4f4';
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(e.target.id);
+    // console.log(e.target.className);
+    // console.log(e.target.classList);
+
+    // var output=document.getElementById('output');
+    // output.innerHTML='<h3>'+e.target.id+'</h3>';
+    // console.log(e.type);
+
+    // console.log(e.clientX);
+    // console.log(e.clientY);
+
+    // console.log(e.offsetX);
+    // console.log(e.offsetY);
+
+    // console.log(e.altKey);
+    // console.log(e.ctrlKey);
+    // console.log(e.shiftKey);
+
+
+}
