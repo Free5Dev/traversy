cb063b706d9fdfd6f73ed3e58e548cdc9da30d82
Thierno SOUMAH <thsoum@ad.phedre.fr>
2019-09-25 15:48:44 +0200

    dom3

--- dom.js
+++ dom.js
@@ -230,3 +230,85 @@ console.log(h1);
 
 newDiv.style.fontSize='30px';
 container.insertBefore(newDivText, h1);
+
+// -------------------------------------------------dom 3--------------
+// event 
+var button = document.getElementById('button').addEventListener('click', buttonClick); //select le btn ensuite ajout d'écouteur d'evenement 
+
+function buttonClick(e){ //function applé ausein de l'ecouteur d'evenement 
+    console.log('On clicked');
+    document.getElementById('header-title').textContent='Changed';
+    document.querySelector('#main').style.backgroundColor='#f4f4f4';
+
+    console.log(e);
+
+    console.log(e.target);
+
+    console.log(e.target.id);
+
+    console.log(e.target.className);
+
+    console.log(e.target.classList);
+
+    var output=document.getElementById('output');
+    output.innerHTML='<h3>'+e.target.id+'</h3>';
+
+    console.log(e.type);
+
+    // console.log(e.clientX);
+    // console.log(e.clientY);
+
+    // console.log(e.offsetX);
+    // console.log(e.offsetY);
+
+    // console.log(e.altKey);
+    // console.log(e.ctrKey);
+    // console.log(e.shiftKey);
+}
+
+var button2=document.getElementById('button2');
+button2.addEventListener('click', runEvent);
+button2.addEventListener('dblclick', runEvent);
+button2.addEventListener('mousedown', runEvent);
+button2.addEventListener('mouseup', runEvent);
+
+var box = document.getElementById('box');
+
+box.addEventListener('mouseenter', runEvent);
+box.addEventListener('mouseover', runEvent);
+box.addEventListener('mouveleave', runEvent);
+box.addEventListener('mouseover', runEvent);
+box.addEventListener('mouseout', runEvent);
+box.addEventListener('mousemove', runEvent);
+
+var itemInput=document.querySelector('input[type="text"]');
+console.log(itemInput);
+var form=document.querySelector('form');
+console.log(form);
+itemInput.addEventListener('keydown', runEvent);
+// itemInput.addEventListener('keypress', runEvent);
+// itemInput.addEventListener('keyup', runEvent);
+
+// itemInput.addEventListener('focus', runEvent);
+// itemInput.addEventListener('paste', runEvent);
+
+var select=document.querySelector('select');
+select.addEventListener('change', runEvent);
+select.addEventListener('input', runEvent);
+
+form.addEventListener('submit', runEvent);
+
+function runEvent(e){
+
+    e.preventDefault();
+
+    console.log('Event Type: '+e.type);
+    console.log(e.target.value);
+
+    output.innerHTML='<h3>MouseX'+e.offsetX+'</h3><h3>MouseY'+e.offsetY+'</h3>';
+    // box.style.backgroundColor="rgb("+e.offsetX+", "+e.offsetY+", 40)";
+    document.body.style.backgroundColor="rgb("+e.offsetX+", "+e.offsetY+", 40)";
+    console.log(e.target.value);
+
+    output.innerHTML='<h3>'+e.target.value+'</h3>';
+}
